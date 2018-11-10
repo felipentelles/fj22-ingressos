059bba83f27176be3eaa323a2c1048f90855e672
Felipe <felipentelles@gmail.com>
2018-11-10 11:32:23 -0200

    Mensagem explicando o trabalho que foi feito

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -2,11 +2,25 @@ package br.com.caelum.ingresso.model;
 
 import java.time.LocalTime;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.Entity;
+import org.springframework.data.annotation.Id;
+
+@Entity
 public class Sessao {
 	
+	@Id
+	@GeneratedValue
 	private Integer id;
+	
 	private LocalTime horario;
+	
+	@ManyToOne
 	private Sala sala;
+	
+	@ManyToOne
 	private Filme filme;
 	
 	public Sessao(LocalTime horario, Filme filme, Sala sala){
